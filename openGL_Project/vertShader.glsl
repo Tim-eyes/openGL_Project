1454a2601670df782df7ca08966a5da020ddf62c
Tim-eyes <18228057503@163.com>
2020-12-06 22:26:40 +0800

    shade

--- openGL_Project/vertShader.glsl
+++ openGL_Project/vertShader.glsl
@@ -2,13 +2,46 @@
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 tex_coord;
+layout (location = 2) in vec3 vertNormal;
+
 out vec2 tc;
+out vec3 varyingNormal;
+out vec3 varyingLightDir;
+out vec3 varyingVertPos;
+out vec3 varyingHalfVector;
+
+struct PositionalLight
+{	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec3 position;
+};
+struct Material
+{	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	float shininess;
+};
+
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 norm_matrix;
 
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 layout (binding=0) uniform sampler2D s;
 
 void main(void)
-{	gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
+{	
+	varyingVertPos = (mv_matrix * vec4(position,1.0)).xyz;
+	varyingLightDir = light.position - varyingVertPos;
+	varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz;
+	
+	varyingHalfVector =
+		normalize(normalize(varyingLightDir)
+		+ normalize(-varyingVertPos)).xyz;
+
+	gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
 	tc = tex_coord;
 }
