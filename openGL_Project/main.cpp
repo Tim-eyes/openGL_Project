0d2eb795cdbe569da19883becb787e803cdd197a
Tim-eyes <18228057503@163.com>
2020-12-06 22:24:46 +0800

    shade

--- openGL_Project/main.cpp
+++ openGL_Project/main.cpp
@@ -26,6 +26,7 @@ GLuint vbo[numVBOs];
 float rotAmt = 0.0f;
 bool keys[100000];
 
+
 //camera
 GLfloat lastFrameTime = 0.0f;
 GLfloat deltaTime = 0.0f;
@@ -34,21 +35,67 @@ glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 glm::vec3 cameraPos = glm::vec3(cameraX, cameraY, cameraZ);
 
 // variable allocation for display
-GLuint vLoc, mvLoc, projLoc;
+GLuint vLoc, mvLoc, projLoc, nLoc;
+GLuint globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mambLoc, mdiffLoc, mspecLoc, mshiLoc;
+float lightPos[3];
 int width, height;
 float aspect;
-glm::mat4 pMat, vMat, mMat, mvMat;
-
+glm::mat4 pMat, vMat, mMat, mvMat,invTrMat, rMat;
 stack<glm::mat4> mvStack;
+
 GLuint sunTexture, planetTexture, moonTexture, jupiterTexture, marsTexture, 
 mercuryTexture, neptuneTexture, plutoTexture, saturnTexture, uranusTexture, venusTexture,
 backgroundTexture;
 
+glm::vec3 currentLightPos, transformed;
+glm::vec3 lightLoc = glm::vec3(5.0f, 2.0f, 2.0f);
 GLfloat LightPosition[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
+// white light
+float globalAmbient[4] = { 0.7f, 0.7f, 0.7f, 1.0f };
+float lightAmbient[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+float lightDiffuse[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+float lightSpecular[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+// gold material
+float* matAmb = Utils::goldAmbient();
+float* matDif = Utils::goldDiffuse();
+float* matSpe = Utils::goldSpecular();
+float matShi = Utils::goldShininess();
+
 Sphere mySphere = Sphere(48);
-Torus myTorus = Torus();
-int numTorusVertices, numTorusIndices;
+Torus myTorus = Torus(0.5f, 0.2f, 48);
+//int numTorusVertices, numTorusIndices;
+float toRadians(float degrees) { return (degrees * 2.0f * 3.14159f) / 360.0f; }
+
+void installLights(glm::mat4 vMatrix) {
+	transformed = glm::vec3(vMatrix * glm::vec4(currentLightPos, 1.0));
+	lightPos[0] = transformed.x;
+	lightPos[1] = transformed.y;
+	lightPos[2] = transformed.z;
+
+	// get the locations of the light and material fields in the shader
+	globalAmbLoc = glGetUniformLocation(renderingProgram, "globalAmbient");
+	ambLoc = glGetUniformLocation(renderingProgram, "light.ambient");
+	diffLoc = glGetUniformLocation(renderingProgram, "light.diffuse");
+	specLoc = glGetUniformLocation(renderingProgram, "light.specular");
+	posLoc = glGetUniformLocation(renderingProgram, "light.position");
+	mambLoc = glGetUniformLocation(renderingProgram, "material.ambient");
+	mdiffLoc = glGetUniformLocation(renderingProgram, "material.diffuse");
+	mspecLoc = glGetUniformLocation(renderingProgram, "material.specular");
+	mshiLoc = glGetUniformLocation(renderingProgram, "material.shininess");
+
+	//  set the uniform light and material values in the shader
+	glProgramUniform4fv(renderingProgram, globalAmbLoc, 1, globalAmbient);
+	glProgramUniform4fv(renderingProgram, ambLoc, 1, lightAmbient);
+	glProgramUniform4fv(renderingProgram, diffLoc, 1, lightDiffuse);
+	glProgramUniform4fv(renderingProgram, specLoc, 1, lightSpecular);
+	glProgramUniform3fv(renderingProgram, posLoc, 1, lightPos);
+	glProgramUniform4fv(renderingProgram, mambLoc, 1, matAmb);
+	glProgramUniform4fv(renderingProgram, mdiffLoc, 1, matDif);
+	glProgramUniform4fv(renderingProgram, mspecLoc, 1, matSpe);
+	glProgramUniform1f(renderingProgram, mshiLoc, matShi);
+}
 void setupVertices(void) {
 	float cubeVertexPositions[108] =
 	{ -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
@@ -104,18 +151,57 @@ void setupVertices(void) {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertexPositions), cubeVertexPositions, GL_STATIC_DRAW);
 }
 
+void setupVerticesTorus(void) {
+	std::vector<int> ind = myTorus.getIndices();
+	std::vector<glm::vec3> vert = myTorus.getVertices();
+	std::vector<glm::vec2> tex = myTorus.getTexCoords();
+	std::vector<glm::vec3> norm = myTorus.getNormals();
+
+	std::vector<float> pvalues;
+	std::vector<float> tvalues;
+	std::vector<float> nvalues;
+
+	for (int i = 0; i < myTorus.getNumVertices(); i++) {
+		pvalues.push_back(vert[i].x);
+		pvalues.push_back(vert[i].y);
+		pvalues.push_back(vert[i].z);
+		tvalues.push_back(tex[i].s);
+		tvalues.push_back(tex[i].t);
+		nvalues.push_back(norm[i].x);
+		nvalues.push_back(norm[i].y);
+		nvalues.push_back(norm[i].z);
+	}
+
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[4]);
+	glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[5]);
+	glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[6]);
+	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[7]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * 4, &ind[0], GL_STATIC_DRAW);
+}
+
 void init(GLFWwindow* window) {
 	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
 	renderingProgramCubeMap = Utils::createShaderProgram("vertCubeShader.glsl", "fragCubeShader.glsl");
 
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 20.0f;
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 100.0f;
 	sphLocX = 0.0f; sphLocY = 0.0f; sphLocZ = -1.0f;
-	torLocX = 0.0f; torLocY = 0.0f; torLocZ = -1.0f;
+	torLocX = 0.0f; torLocY = 0.0f; torLocZ = -0.5f;
 	glfwGetFramebufferSize(window, &width, &height);
 	aspect = (float)width / (float)height;
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
 
 	setupVertices();
+
 
 	//load map to cube as background
 	backgroundTexture = Utils::loadCubeMap("background");
@@ -188,15 +274,25 @@ void display(GLFWwindow* window, double currentTime) {
 
 	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
-
+	nLoc = glGetUniformLocation(renderingProgram, "norm_matrix");
+	
 	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(sphLocX, sphLocY, sphLocZ));
+	currentLightPos = glm::vec3(lightLoc.x, lightLoc.y, lightLoc.z);
+	rotAmt += 0.5f;
+	rMat = glm::rotate(glm::mat4(1.0f), toRadians(rotAmt), glm::vec3(0.0f, 0.0f, 1.0f));
+	currentLightPos = glm::vec3(rMat * glm::vec4(currentLightPos, 1.0f));
+	
+	installLights(vMat);
+	
 	mvMat = vMat * mMat;
+	invTrMat = glm::transpose(glm::inverse(mvMat));
+	
 	mvStack.push(mvMat);
 
 	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-
+	glUniformMatrix4fv(nLoc, 1, GL_FALSE, glm::value_ptr(invTrMat));
+	
 	//sun
 	mvStack.push(mvStack.top());
 	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
@@ -204,7 +300,6 @@ void display(GLFWwindow* window, double currentTime) {
 	mvStack.top() *= rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0, 1.0, 0.0));
 	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
 	
-
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 	glEnableVertexAttribArray(0);
@@ -384,6 +479,8 @@ void display(GLFWwindow* window, double currentTime) {
 	glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
 	mvStack.pop(); mvStack.pop();
 
+	//Saturn Torus
+
 	//Uranus
 	mvStack.push(mvStack.top());
 	mvStack.top() *= scale(glm::mat4(1.0f), glm::vec3(0.42f, 0.42f, 0.42f));
@@ -431,6 +528,42 @@ void display(GLFWwindow* window, double currentTime) {
 	mvStack.pop();
 }
 
+void displayTorus(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	glm::mat4 vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	glm::mat4 mMat = glm::translate(glm::mat4(1.0f), glm::vec3(torLocX, torLocY, torLocZ));
+	//mMat *= glm::eulerAngleXYZ(toRadians(30.0f), 0.0f, 0.0f);
+	mMat = glm::rotate(mMat, toRadians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+	glm::mat4 mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[4]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[5]);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[7]);
+	glDrawElements(GL_TRIANGLES, myTorus.getIndices().size(), GL_UNSIGNED_INT, 0);
+
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
@@ -468,7 +601,7 @@ int main(int argc,char **argv) {
 	init(window);
 
 	while (!glfwWindowShouldClose(window)) {
-		
+	
 		display(window, glfwGetTime());
 		glfwSwapBuffers(window);
 		glfwPollEvents();
